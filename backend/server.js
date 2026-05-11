5b4f71ec8e95c0ef359ea221a15f722468a6fda3
Testerzopa <testerzopa111@gmail.com>
2026-05-11 16:48:35 +0700

    First Post data

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,14 @@ const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/test', (req, res) => {
+  res.json('This is Test')
 })
 
 app.listen(port, () => {
