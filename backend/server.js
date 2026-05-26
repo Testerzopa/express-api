70f9f910750de5486a163c5f133d8ef7a1ad5bd8
Testerzopa <testerzopa111@gmail.com>
2026-05-26 08:32:24 +0700

    Make database for post and get data

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,15 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+let dataBase = []
+
+app.get('/test', (req, res) => {
+  res.json(dataBase)
 })
 
 app.post('/test', (req, res) => {
-  res.json('This is Test')
+  dataBase = req.body
+  res.json({message:"บันทึกแล้ว",data:dataBase})
 })
 
 app.listen(port, () => {
